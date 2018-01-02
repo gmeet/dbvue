ef6937612958684cdeca64cb3b9c166fa440f484
gmeet <1106056044@qq.com>
2018-01-02 18:41:31 +0800

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
   },
   env: {
     browser: true,
+    es6: true
   },
   // https://github.com/standard/standard/blob/master/docs/RULES-en.md
   extends: 'standard',
@@ -20,6 +21,7 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 'off',
     'no-useless-escape': 'off',
+    'no-unused-vars': 'off', //未使用过的变量不能声明 在函数外部作用域定义的变量内部访问报错
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
